039bb50f985b503d96fee7ef77dbb2a30004527f
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2022-02-13 19:47:44 +0100

    Firts commit

---
+++ public/app.js
@@ -0,0 +1,72 @@
+var word = '';
+var row = 1;
+
+const keys = [
+  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+  ['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'backspace']
+];
+
+comp_word = () => {
+  $().get('http://localhost:5000/checkword', {word, word}, (resp) => {
+    if (resp) {
+      for(i=0; i < 5; i++){
+        const nth = i+1;
+        letter = resp[i];
+        col = $(`#row-${row} .letter:nth-of-type(${nth})`);
+        col.removeClass('correct other-pos no-letter');
+        if (letter.samepos == 1) col.addClass('correct');
+        else if (letter.samepos == 0) col.addClass('other-pos');
+        else if (letter.samepos == -1) col.addClass('no-letter');
+      }
+
+      if (row < 6) row++;
+      word = '';
+    }
+  });
+}
+
+comp_key = key => {
+  key = key.toLowerCase();
+  let keycode = key.charCodeAt();
+  
+  if (key == 'enter' && word.length == 5) {
+    comp_word();
+  } else if (key == 'backspace') {
+    if (word.length > 0) word = word.slice(0, -1);
+  } else {
+    if (keycode >= 97 && keycode <= 122 && word.length < 5) word += key;
+  }
+    
+  for (i=0; i < 5; i++) {
+    const nth = i+1;
+    if (i < word.length) $(`#row-${row} .letter:nth-of-type(${nth})`).text(word[i]);
+    else $(`#row-${row} .letter:nth-of-type(${nth})`).text(' ');
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  $().get('http://localhost:5000/getwordday', {}, (resp) => {});
+
+  var keys_html = '';
+  keys.forEach( r => {
+    keys_html += '<div class="keys-row">'
+    r.forEach( l => {
+      if (l == 'enter') keys_html += `<div id="key-enter" class="key" data-key="${l}"></div>`;
+      else if (l == 'backspace') keys_html += `<div id="key-backspace" class="key" data-key="${l}"></div>`;
+      else keys_html += `<div class="key" data-key="${l}">${l}</div>`;
+    });
+    keys_html += '</div>'
+    $('#keyboard').html(keys_html);
+  });
+
+  $('body').on('keyup', e => {
+    const key = e.key.toLowerCase();
+    comp_key(key);
+  });
+
+  $('#keyboard').on('click', '.key', e => {
+    const key = $(e.target).attr('data-key');
+    comp_key(key);
+  });
+});
---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const {createReadStream} = require('fs');
+const readline = require('readline');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(helmet());
+app.use(express.static('public'));
+
+var words = [];
+var currentWord = '';
+
+const readLines = readline.createInterface({
+  input: createReadStream('words.txt')
+});
+
+readLines.on('line', word => {
+  words.push(word);
+});
+
+wordOfTheDay = () => {
+  const random = Math.floor(Math.random() * words.length);
+  return words[random];
+}
+
+resolveWord = word => {
+  let resolve = [];
+  // Split the current word
+  const cw_letters = currentWord.split('');
+  const w_letter = word.split('');
+  for (i=0; i < w_letter.length; i++) {
+    if (w_letter[i] == cw_letters[i]) {
+      resolve.push({
+        'letter'	: w_letter[i],
+        'samepos'	: 1
+      });
+    } else {
+      const positions = letterPositions(w_letter[i]);
+      if (positions.length > 0) {
+        resolve.push({
+          'letter'	: w_letter[i],
+          'samepos'	: 0
+        });
+      } else {
+        resolve.push({
+          'letter'	: w_letter[i],
+          'samepos'	: -1
+        });
+      }
+    }
+  }
+  return resolve;
+}
+
+letterPositions = letter => {
+  var a=[],i=-1;
+  while((i=currentWord.indexOf(letter,i+1)) >= 0) a.push(i);
+  return a;
+}
+
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  createReadStream('index.html').pipe(res)
+});
+
+app.get('/getwordday', (req, res) => {
+  const word = wordOfTheDay();
+  currentWord = word
+  res.json(word);
+});
+
+app.get('/checkword', (req, res) => {
+  if (req.query.word) {
+    let resolve = resolveWord(req.query.word);
+    res.json(resolve);
+  } else res.end('No word');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
